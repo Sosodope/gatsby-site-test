8cfbe57124a637f9c1ce67e912d0505864375d00
Sosodope <sonadolkar93@hotmail.com>
2018-09-15 23:14:36 +0700

    Add more components and create service section

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,22 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
+import logo from "../images/logo-cactus.svg";
+import "./header.css";
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/">
+        <img width="50" src={logo} />
+      </Link>
+      <Link to="/courses">About Us</Link>
+      <Link to="/downloads">Services</Link>
+      <Link to="/workshops">Pricing</Link>
+      <Link to="/buy">
+        <button className="Header-btn">Contact</button>
+      </Link>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
